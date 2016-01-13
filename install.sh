7e51372e5ffb96bdb19e5fe6db71504e7a64e54c
Igor Dyatlov <dyatlov.igor@gmail.com>
2016-01-13 13:45:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ read -p "Do you want to install Placmank [Y/n]?" yn
 if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
 
     echo "Copying plank theme..."
-    cp -Ri Placmank /usr/share/plank/themes
-
+    cp -Ri /Placmank /usr/share/plank/themes
     echo
     echo "Done"
 fi
