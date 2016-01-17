0835247268ddd8053e8c531e25a4d65b267cc5e7
Igor Dyatlov <dyatlov.igor@gmail.com>
2016-01-17 16:02:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-read -p "Do you want to install Placmank [Y/n]?" yn
-
-if [[ $yn =~ ^[Yy]$ ]] || [[ $yn == "" ]]; then
-
-    echo "Copying plank theme..."
-    show_warning 'Requires root privileges'
-    sudo cp -Ri Placmank /usr/share/plank/themes
-    echo
-    show_success 'Done.'
-    echo "Done"
-fi
+    cp -Ri Placmank /usr/share/plank/themes
